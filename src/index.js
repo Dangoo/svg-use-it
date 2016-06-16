de9c194dc869560e0401ab48a3e979c138b67aec
dangoo <daniel.gooss@sinnerschrader.com>
2016-06-16 19:03:46 +0200

    Improved script to replace use

--- src/index.js
+++ src/index.js
@@ -1,77 +1,111 @@
-// Anchor element as parser for URls as stated here: https://gist.github.com/jlong/2428561
-const parser = document.createElement('a');
-
-// Cache for already inlined files
-let inlinedCache = [];
+// anchor element as parser for URls as stated here: https://gist.github.com/jlong/2428561
+var parser = document.createElement('a');
 
 /**
- * @param  {String} filePath File path
- * @return {Boolean}         File already inlined
- */
-function inCache(path) {
-	return Boolean(inlinedCache.indexOf(path) !== -1);
-}
-
-/**
+ * @param  {Object}        items   Previous returned value, in this case Object with use elements
  * @param  {SVGUseElement} element <use> element
- * @param  {String}        xlink   Xlink attribute
+ * @return {Object}                Object with sorted use elements
  */
-function changeXlink(element, xlink) {
-	element.setAttribute('xlink:href', xlink);
-}
-
-/**
- * @param  {Array}         files   Previous returned value, in this case Array of file paths
- * @param  {SVGUseElement} element Xlink attribute
- * @return {Array}                 File paths to SVG resources
- */
-function processItems(files, element) {
+function sortItems(items, element) {
 	parser.href = element.getAttribute('xlink:href');
 
-	const filePath = parser.pathname;
-	if (filePath) {
-		if (files.indexOf(filePath) === -1) {
-			files.push(filePath);
-		}
+	var filePath = parser.pathname;
 
-		changeXlink(element, parser.hash);
+	var hash = parser.hash;
+
+	if (!items[filePath]) {
+		items[filePath] = {
+			nodes: [],
+			fragments: []
+		};
 	}
 
-	return files;
+	var item = items[filePath];
+
+	item.nodes.push({
+		element: element,
+		fragment: hash
+	});
+
+	if (item.fragments.indexOf(hash) === -1) {
+		item.fragments.push(hash);
+	}
+
+	return items;
 }
 
 /**
- * @param  {Element | String} content Content to insert
+ * @param  {SVGSVGElement}    doc SVG document
+ * @param  {Array}            ids Fragment ids
+ * @return {Array}            SVG symbols
  */
-function inlineFile(content) {
-	const body = document.body;
-	body.insertBefore(content, body.firstChild);
+function getSymbols(doc, ids) {
+	return Array
+		.prototype
+		.slice
+		.call(doc.querySelectorAll(ids.join(',')))
+		.reduce(function (items, item) {
+			var key = ['#', item.id].join('');
+			items[key] = item;
+			return items;
+		}, {});
+}
+
+/**
+ * @param  {SVGUseElement}    item    SVG element to be replaced
+ * @param  {SVGSymbolElement} content SVG contents to replace <use>
+ */
+function embed(item, content) {
+	var parent = item.parentNode;
+
+	// Setting viewBox attribute if not already set
+	if (!parent.getAttribute('viewBox')) {
+		parent.setAttribute('viewBox', content.getAttribute('viewBox'));
+	}
+
+	// creating new fragment as a store for the contents children
+	var fragment = document.createDocumentFragment();
+	var copy = content.cloneNode(true);
+
+	// move each child to fragment
+	while (copy.childNodes.length) {
+		fragment.appendChild(copy.firstChild);
+	}
+
+	// replace use element by svg content
+	parent.replaceChild(fragment, item);
 }
 
 /**
  * Callback for XHR request
  */
-function fileLoaded() {
-	const response = this.response;
+function fileLoaded(xhr, resourceFiles) {
+	var response = xhr.response;
+
 	if (
-		!inCache(this.responseUrl) &&
-		this.readyState === 4 &&
+		xhr.readyState === 4 &&
 		response &&
 		response.contentType === 'image/svg+xml'
 	) {
-		inlineFile(response.documentElement);
-		inlinedCache.push(this.responseUrl);
+		var symbols = getSymbols(
+			response.documentElement,
+			resourceFiles.fragments
+		);
+
+		resourceFiles.nodes.forEach(function (item) {
+			embed(item.element, symbols[item.fragment]);
+		});
 	}
 }
 
 /**
  * @param  {String} filePath File path to request
  */
-function fetchFile(filePath) {
-	// Using XMLHttpRequest here due to lack of fetch support in browsers not supporting fragments
-	const xhr = new XMLHttpRequest();
+function fetchFile(filePath, callback) {
+	// using XMLHttpRequest here due to lack of fetch support in browsers not supporting fragments
+	var xhr = new XMLHttpRequest();
 
-	xhr.addEventListener('load', fileLoaded);
+	xhr.addEventListener('load', callback);
 	xhr.addEventListener('error', console.error);
 	xhr.open('GET', filePath);
 	xhr.responseType = 'document';
@@ -79,29 +113,59 @@ function fetchFile(filePath) {
 }
 
 /**
- * @param {String} root      Entry component
- * @param {Array}  blacklist Blacklist for child nodes
+ * @param {String} rootSelector Entry component
+ * @param {Array}  blacklist    Blacklist for child nodes
+ * @param {String} query    	Query selector to search for
  */
-export default function(rootSelector = 'body', blacklist = []) {
-	// Element name to query for
-	const query = 'use'
-
+function init(rootSelector, blacklist, query) {
 	// building the negotiation for blacklisting
-	const negotiations = blacklist.map(item => {
+	var negotiations = blacklist.map(item => {
 		return [':not(', item, ')'].join('');
 	}).join('');
 
 	// complete seletor for elements
-	const selector = negotiations ?
+	var selector = negotiations ?
 		[rootSelector, '>', negotiations, query].join(' ') :
 		[rootSelector, query].join(' ');
-	const root = document.querySelector(rootSelector);
-	const matches = root.querySelectorAll(selector) || [];
-	const resourceFiles = Array
+	var root = document.querySelector(rootSelector);
+	var matches = root.querySelectorAll(selector);
+	var resourceFiles = Array
 		.prototype
 		.slice
 		.call(matches)
-		.reduce(processItems, []);
+		.reduce(sortItems, {});
 
-	resourceFiles.forEach(fetchFile);
+	Object.keys(resourceFiles).forEach(function (key) {
+		var items = resourceFiles[key];
+		fetchFile(key, function () {
+			fileLoaded(this, items);
+		});
+	});
+}
+
+/**
+ * @param {HTMLElement} testNode
+ */
+function supportsExternalFragments(testNode) {
+	return testNode.getBoundingClientRect().width > 0;
+}
+
+/**
+ * @param {String} rootSelector Entry component
+ * @param {Array}  blacklist    Blacklist for child nodes
+ */
+export default function(rootSelector, blacklist) {
+	// element name to query for
+	var query = 'use';
+	var supportsExternalFragments = supportsExternalFragments(
+		document.querySelector(query)
+	);
+
+	if (!supportsExternalFragments) {
+		// setting default values
+		rootSelector = rootSelector || 'body';
+		blacklist = blacklist || [];
+
+		init(rootSelector, blacklist, query);
+	}
 }
